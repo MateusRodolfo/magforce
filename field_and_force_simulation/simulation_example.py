e32b60eb3443d6036a52148af774b1d187c2e326
rods-rodolfo <66533348+rods-rodolfo@users.noreply.github.com>
2020-08-03 14:41:24 +0200

    03 August 2020 14h40 - added saveCSV option to functions

--- field_and_force_simulation/simulation_example.py
+++ field_and_force_simulation/simulation_example.py
@@ -13,9 +13,9 @@ ys = linspace(-30, 30, 1000)
 zs = linspace(0, 30, 1000)
 x, y, z = (0, 0, 0)
 
-# pf.plot_1D_along_x(xs, y, z, my_collection, sample=sample, BF='BF')
-# pf.plot_1D_along_y(x, ys, z, my_collection, sample=sample, BF='BF')
-# pf.plot_1D_along_z(x, y, zs, my_collection, sample=sample, BF='BF')
+# pf.plot_1D_along_x(xs, y, z, my_collection, sample=sample, BF='BF', saveCSV=False)
+# pf.plot_1D_along_y(x, ys, z, my_collection, sample=sample, BF='BF', saveCSV=False)
+# pf.plot_1D_along_z(x, y, zs, my_collection, sample=sample, BF='BF', saveCSV=False)
 
 
 # Plotting 2D
@@ -24,15 +24,15 @@ ys = linspace(-30, 30, 35)
 zs = linspace(0, 30, 35)
 x, y, z = (0, 0, 0)
 
-# pf.plot_2D_plane_x(x, ys, zs, my_collection, sample=sample, modes=['stream', 'quiver', 'surface'], BF='BF')
-# pf.plot_2D_plane_y(xs, y, zs, my_collection, sample=sample, modes=['stream', 'quiver', 'surface'], BF='BF')
-# pf.plot_2D_plane_z(xs, ys, z, my_collection, sample=sample, modes=['stream', 'quiver', 'surface'], BF='BF')
+# pf.plot_2D_plane_x(x, ys, zs, my_collection, sample=sample, modes=['stream', 'quiver', 'surface'], BF='BF', saveCSV=False)
+# pf.plot_2D_plane_y(xs, y, zs, my_collection, sample=sample, modes=['stream', 'quiver', 'surface'], BF='BF', saveCSV=False)
+# pf.plot_2D_plane_z(xs, ys, z, my_collection, sample=sample, modes=['stream', 'quiver', 'surface'], BF='BF', saveCSV=False)
 
 # Plotting 3D
 xs = linspace(-30, 30, 7)
 ys = linspace(-30, 30, 7)
 zs = linspace(0, 30, 7)
 
-# pf.plot_3D(xs, ys, zs, my_collection, sample=sample, BF='BF')
+# pf.plot_3D(xs, ys, zs, my_collection, sample=sample, BF='BF', saveCSV=False)
 
 show()
