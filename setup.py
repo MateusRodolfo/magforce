abae13abda629f6a6c7161b368a0e34d7d7e4794
rods-rodolfo <mateus.rodolfo.1998@gmail.com>
2020-08-08 13:21:03 +0200

    v2.2

--- setup.py
+++ setup.py
@@ -3,28 +3,33 @@
 # twine upload dist/*
 
 from distutils.core import setup
+
+with open("README.md", "r") as readme:
+  long_description = readme.read()
+
 setup(
-  name = 'magforce',         # How you named your package folder (MyLib)
-  packages = ['magforce'],   # Chose the same as "name"
-  version = '2.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'free python3 library for easy calculation and plotting of magnetic forces',   # Give a short description about your library
-  author = 'Mateus Rodolfo',                   # Type in your name
-  author_email = 'mateusgrodolfo@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/MateusRodolfo/magforce',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/MateusRodolfo/magforce/archive/v2.1.tar.gz',    # I explain this later on
-  keywords = ['python', 'python3', 'magnet'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  name = 'magforce',
+  packages = ['magforce'],
+  version = '2.2',
+  license='agpl-3.0',
+  description = 'Free Python3 library for easy calculation and plotting of magnetic forces',
+  long_description=long_description,
+  author = 'Mateus Rodolfo',
+  author_email = 'mateusgrodolfo@gmail.com',
+  url = 'https://github.com/MateusRodolfo/magforce',
+  download_url = 'https://github.com/MateusRodolfo/magforce/archive/v2.2.tar.gz',
+  keywords = ['python', 'python3', 'magnet'],
+  install_requires=[
           'magpylib',
           'numpy',
           'matplotlib',
       ],
   classifiers=[
-    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'LICENSE :: OSI APPROVED :: GNU AFFERO GENERAL PUBLIC LICENSE V3',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
