b567365cd24213d775968f2ef0922d108adfe129
rods-rodolfo <mateus.rodolfo.1998@gmail.com>
2020-08-08 13:39:01 +0200

    v2.4

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@
 
 from distutils.core import setup
 
-with open("README.md", "r") as readme:
-  long_description = readme.read()
+long_description = 'Free Python3 library for easy calculation and plotting of magnetic forces.\n\n' \
+                   'Made by [Mateus Rodolfo](https://www.linkedin.com/in/mateusgrodolfo/) during an internship at LNCMI (CNRS) supervised by Eric Beaugnon.\n\n' \
+                   'Uses the [magpylib](https://www.sciencedirect.com/science/article/pii/S2352711020300170) python library.\n\n' \
+                   'Thanks [Mr Ortner](https://www.linkedin.com/in/michael-ortner-b6b724143/) for all  the help provided.\n\n' \
+                   '[Examples files](https://github.com/MateusRodolfo/magforce/tree/master/examples) using the source code (no imports), using the library as an import as well as magpylib examples for magnet setup.'
 
 setup(
   name = 'magforce',
@@ -30,8 +33,6 @@ setup(
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: GNU Affero General Public License v3',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
