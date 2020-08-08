44d97d79b1c2580d664691207570d9abf087160b
rods-rodolfo <mateus.rodolfo.1998@gmail.com>
2020-08-08 14:07:11 +0200

    v2.5

--- setup.py
+++ setup.py
@@ -5,22 +5,21 @@
 from distutils.core import setup
 
 long_description = 'Free Python3 library for easy calculation and plotting of magnetic forces.\n\n' \
-                   'Made by [Mateus Rodolfo](https://www.linkedin.com/in/mateusgrodolfo/) during an internship at LNCMI (CNRS) supervised by Eric Beaugnon.\n\n' \
-                   'Uses the [magpylib](https://www.sciencedirect.com/science/article/pii/S2352711020300170) python library.\n\n' \
-                   'Thanks [Mr Ortner](https://www.linkedin.com/in/michael-ortner-b6b724143/) for all  the help provided.\n\n' \
-                   '[Examples files](https://github.com/MateusRodolfo/magforce/tree/master/examples) using the source code (no imports), using the library as an import as well as magpylib examples for magnet setup.'
+                   'Made by Mateus Rodolfo during an internship at LNCMI (CNRS) supervised by Eric Beaugnon.\n\n' \
+                   'Uses the magpylib python library. Thanks Mr Ortner for all the help provided.'
+
 
 setup(
   name = 'magforce',
   packages = ['magforce'],
-  version = '2.4',
-  license='agpl-3.0',
+  version = '2.5',
+  license = 'agpl-3.0',
   description = 'Free Python3 library for easy calculation and plotting of magnetic forces',
-  long_description=long_description,
+  long_description = long_description,
   author = 'Mateus Rodolfo',
   author_email = 'mateusgrodolfo@gmail.com',
   url = 'https://github.com/MateusRodolfo/magforce',
-  download_url = 'https://github.com/MateusRodolfo/magforce/archive/v2.4.tar.gz',
+  download_url = 'https://github.com/MateusRodolfo/magforce/archive/v2.5.tar.gz',
   keywords = ['python', 'python3', 'magnet'],
   install_requires=[
           'magpylib',
@@ -30,7 +29,13 @@ setup(
   classifiers=[
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Education',
+    'Natural Language :: English',
+    'Topic :: Education',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Physics',
     'License :: OSI Approved :: GNU Affero General Public License v3',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
@@ -38,3 +43,4 @@ setup(
     'Programming Language :: Python :: 3.8',
   ],
 )
+
