3171253e27e3a0ac5d1454de0ecfa8882a1ccdcd
rods-rodolfo <mateus.rodolfo.1998@gmail.com>
2021-07-08 15:21:47 +0200

    v3.3

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ long_description = 'Free Python3 library for easy calculation and plotting of ma
 setup(
   name = 'magforce',
   packages = ['magforce'],
-  version = '3.2',
+  version = '3.3',
   license = 'agpl-3.0',
   description = 'Free Python3 library for easy calculation and plotting of magnetic forces',
   long_description = long_description,
   author = 'Mateus Rodolfo',
   author_email = 'mateusgrodolfo@gmail.com',
   url = 'https://github.com/MateusRodolfo/magforce',
-  download_url = 'https://github.com/MateusRodolfo/magforce/archive/v3.2.tar.gz',
+  download_url = 'https://github.com/MateusRodolfo/magforce/archive/refs/tags/3.3.tar.gz',
   keywords = ['python', 'python3', 'magnet'],
   install_requires=[
           'magpylib<3',
